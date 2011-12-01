4c38ddd12fee30d73b55282ecb35bf93237adae4
Jonathan Laver <jonathan.laver@gmail.com>
2011-12-01 12:20:23 +0000

    remove temp dir

--- Student Robotics.py
+++ Student Robotics.py
@@ -148,6 +148,7 @@ class DeployZipCommand(sublime_plugin.WindowCommand):
 				theZip = self.makeZip(userPaths[0], pyenvLocation, ignorePatterns)
 				target = os.path.join(drive["path"], "robot.zip")
 				shutil.copyfile(theZip, target)
+				shutil.rmtree(self.tmpd)
 				sublime.status_message("Zip deployed successfully to %s!"%target)
 
 		self.window.show_quick_panel(messages, onDriveChosen)
